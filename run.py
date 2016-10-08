00aefc988b9fbb4d3af67c7df26ac65a28bdb95e
SufferProgrammer <pijipirma@gmail.com>
2016-10-08 17:37:20 +0700

    add url for API confirmation

--- run.py
+++ run.py
@@ -1,17 +1,32 @@
 from flask import *
 
-
 app = Flask(__name__)
+
+@app.route('/API/<api_key>')
+def responseAPI(api_key):
+    
+    apiResponseOk = [{
+        'Response Status' : 'OK',
+        'Response Code' : 200
+    }]
+    apiResponseError = [{
+        'Response Status' : 'Undefined Error'
+    }]
+    
+    apiAuthKey = ('5C3D8FB361F9F868E5252A76D9484')
+    if api_key == apiAuthKey:
+        return jsonify({'Response': apiResponseOk}), 200
+    else:
+        return jsonify({'Response' : apiResponseError}), 403
+    
 
 @app.route('/')
 def index():
-	return render_template('index.html')
-	
-@app.route('/buildup')
+    return render_template('index.html')
+
+@app.route('/build')
 def buildup():
-	return "<h1><center>THIS PAGE IS UNDER CONSTRUCTION</center></h1>"
-	
+    return 'Under Building'
 
-if __name__ =='__main__':
-
-	app.run(host = '0.0.0.0', debug = True, port = 80)
+if __name__=='__main__':
+    app.run(debug=True, port=7720)
