fe77922636f4adea81f693080dde5ae88ac2ab00
SufferProgrammer <pijipirma@gmail.com>
2016-10-17 16:16:44 +0700

    add some stuff

--- run.py
+++ run.py
@@ -19,10 +19,42 @@ def responseAPI(api_key):
     else:
         return jsonify({'Response' : apiResponseError}), 403
     
+@app.errorhandler(404)
+def errorpage(e):
+    page = 'Not Found'
+    return render_template('error.html', page = page), 404
+
+@app.errorhandler(403)
+def forbiddenpage(e):
+    page = 'Forbidden'
+    return render_template('dame.html', page = page), 403
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    page = 'Index'
+    return render_template('index.html', page = page)
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'GET':
+        whatToSearch = request.args.get('s')
+        if whatToSearch == '':
+            return redirect(url_for('index'))
+        elif whatToSearch != None:
+            page = 'Search'
+            return render_template('search.html', result = whatToSearch, page = page)
+        else:
+            return redirect(url_for('index'))
+
+@app.route('/anime')
+def anime():
+    page = 'Anime Session'
+    return render_template('anime.html', page = page)
+
+@app.route('/manga')
+def manga():
+    page = 'Manga Session'
+    return render_template('manga.html', page = page)
 
 @app.route('/build')
 def buildup():
